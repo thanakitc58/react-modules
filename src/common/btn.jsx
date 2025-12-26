10d8574f14dd45f7fddeeb28930bdf3128db9edc
Pong <vx7vx8vx9@gmail.com>
2025-12-26 12:27:02 +0700

    reac-modules

---
+++ src/common/btn.jsx
@@ -0,0 +1,4 @@
+function Button(props) {
+    return <button className={props.buttonStyle}>{props.buttonText}</button>;
+  }
+  export default Button;
---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <footer>
+        <p>&copy; 2023 Your Ecommerce Store. All rights reserved.</p>
+      </footer>
+    );
+  } 
+  export default Footer;
+ 
